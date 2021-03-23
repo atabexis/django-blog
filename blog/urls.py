52d5e46f5acf23055b90468f6f99795404dfcc97
ABeyshekeeva <abeyshekeeva@beeline.kg>
2021-03-23 20:03:08 +0600

    final files

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [    
     path('', views.post_list, name='post_list'),
     path('authors/', views.author_list, name='author_list'),
+    path('login/', views.user_login, name='login')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .models import Post, Author
-#GET
-#POST 
-def login(request):
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def user_login(request):
     if request.method == 'GET':
-        return render(request, 'blog/login.html')
-    else:
-        
-        pass 
+        form = LoginForm()        
+    else:        
+        print(request.POST)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data #{'username': 'aidai', ;}
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None: 
+                if user.is_active: 
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    messages.error(request, 'Ваш аккаунт заблокирован')
+            else:     
+                messages.error(request, 'Неправильный ввод данных') 
+                          
+    return render(request, 'blog/login.html', {'login_form': form})
 
 def post_list(request):
     # ORM - object relational mapper 
